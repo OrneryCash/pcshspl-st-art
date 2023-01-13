a0d5e2f319fa6c165a23cb7b1b555cd94d581371
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2023-01-13 22:44:13 +0700

    Add Header to the homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Header from "./Header.svelte";
+</script>
+
+<Header />
+<div class="bg-art bg-cover py-64 w-full flex flex-col items-center">
+    <div class="text-white text-6xl text-left font-bold">
+        <h1>PCSHSPL</h1>
+        <h1>Y2K Street Art</h1>
+    </div>
+</div>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,3 @@
+<div class="w-full flex justify-center absolute top-5">
+    <h1 class="text-white">LOGO</h1>
+</div>
