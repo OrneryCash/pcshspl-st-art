71f9bf226aa259e8c0e34f1467d158312b6b665b
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2023-01-14 20:09:51 +0700

    Change vertification method

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,7 @@
     <meta name="keywords" content="Y2K, Street Art, PCSHSPL" />
     <meta name="author" content="Teerapad Pipadboonyarat" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="google-site-verification" content="p0HTzP8dBWOo-_zgLmmo3l9O7u4QCBo-YZILm0I7gdo" />
     <meta charset="utf-8" />
 </svelte:head>
 
