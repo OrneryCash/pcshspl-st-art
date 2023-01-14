f26c556c7735209bbf1089bdb0f54dcc29d101cf
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2023-01-14 11:58:16 +0700

    Add Buttons

---
+++ src/routes/Button.svelte
@@ -0,0 +1,5 @@
+<script>
+    export let room
+    export let active
+</script>
+<button class={`${active && "bg-red-400 text-black"} py-3 px-6 rounded-full border-red-400 text-white border-2`}>6/{room}</button>
