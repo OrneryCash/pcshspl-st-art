5e7c897b50270def2a42d52aec08c3a9e76abf83
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2023-01-14 11:58:58 +0700

    Implement the classroom data

--- src/routes/Gallery.svelte
+++ src/routes/Gallery.svelte
@@ -1,8 +1,38 @@
 <script>
     import Button from "./Button.svelte";
     import Showcase from "./Showcase.svelte";
+    let classroom = [
+        {
+            room: 1,
+            active: true
+        },
+        {
+            room: 2,
+            active: false
+        },
+        {
+            room: 3,
+            active: false
+        },
+        {
+            room: 4,
+            active: false
+        },
+        {
+            room: 5,
+            active: false
+        },
+        {
+            room: 6,
+            active: false
+        },
+    ]
 </script>
-<div class="flex flex-col items-center">
-    <Button />
+<div class="flex flex-col items-center -mt-20">
+    <div class="flex flex-row gap-x-4">
+        {#each classroom as {room, active} (room)}
+            <Button {room} {active} />
+        {/each}
+    </div>
     <Showcase />
 </div>
