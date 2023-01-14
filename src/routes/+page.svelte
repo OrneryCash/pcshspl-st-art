4d7fc66bc0bf66957d96305637cad59b07f4683b
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2023-01-14 19:42:22 +0700

    Add meta

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,14 @@
     import Gallery from "./Gallery.svelte";
 </script>
 
+<svelte:head>
+    <title>PCSHSPL Y2k Art Street</title>
+    <meta name="description" content="PCSHSPL Y2K Street Art">
+    <meta name="keywords" content="Y2K, Street Art, PCSHSPL">
+    <meta name="author" content="Teerapad Pipadboonyarat">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</svelte:head>
+
 <div class="bg-art bg-cover py-64 w-full flex flex-col items-center">
     <Header />
     <div class="text-white text-6xl text-left font-bold">
