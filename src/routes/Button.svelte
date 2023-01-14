38f0e6fc41f7cdd33a77212bb30e75105bc83038
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2023-01-14 13:03:30 +0700

    Add dispatch (Event Listener) to the Button component

--- src/routes/Button.svelte
+++ src/routes/Button.svelte
@@ -1,5 +1,15 @@
 <script>
     export let room
     export let active
+
+    import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher()
+    function sendData() {
+        dispatch('message', {
+            room: room
+        })
+    }
 </script>
-<button class={`${active && "bg-red-400 text-black"} py-3 px-6 rounded-full border-red-400 text-white border-2`}>6/{room}</button>
+
+<button class={`${active && "bg-red-400 text-black"} py-3 px-6 rounded-full border-red-400 text-white border-2`} on:click={sendData}>6/{room}</button>
