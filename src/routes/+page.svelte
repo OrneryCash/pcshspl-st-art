13eca3dcb39c9fe4e336e158516e7760ac557261
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2023-01-14 12:25:44 +0700

    Rearraged the Header to the same as title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,8 @@
     import Gallery from "./Gallery.svelte";
 </script>
 
-<Header />
 <div class="bg-art bg-cover py-64 w-full flex flex-col items-center">
+    <Header />
     <div class="text-white text-6xl text-left font-bold">
         <h1>PCSHSPL</h1>
         <h1>Y2K Street Art</h1>
