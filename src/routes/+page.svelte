28fc90953f9caef918a786f5fc342fc0999ebe7c
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2023-01-14 19:49:05 +0700

    Add meta charset

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,11 @@
 
 <svelte:head>
     <title>PCSHSPL Y2k Art Street</title>
-    <meta name="description" content="PCSHSPL Y2K Street Art">
-    <meta name="keywords" content="Y2K, Street Art, PCSHSPL">
-    <meta name="author" content="Teerapad Pipadboonyarat">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="PCSHSPL Y2K Street Art" />
+    <meta name="keywords" content="Y2K, Street Art, PCSHSPL" />
+    <meta name="author" content="Teerapad Pipadboonyarat" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="utf-8" />
 </svelte:head>
 
 <div class="bg-art bg-cover py-64 w-full flex flex-col items-center">
