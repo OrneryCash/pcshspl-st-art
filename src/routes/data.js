13767df37320e3bd7f92ca68d46c805d558882f9
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2023-01-16 10:12:58 +0700

    Add test art data

---
+++ src/routes/data.js
@@ -0,0 +1,996 @@
+export const art = [
+	{
+		room: 1,
+		number: 1,
+		title: 'Title',
+		description: 'Sample'
+	},
+	{
+		room: 1,
+		number: 2,
+		title: 'Title',
+		description: 'Sample'
+	},
+	{
+		room: 1,
+		number: 3,
+		title: 'Title',
+		description: 'Sample'
+	},
+	{
+		room: 1,
+		number: 4,
+		title: 'Title',
+		description: 'Sample'
+	},
+	{
+		room: 1,
+		number: 5,
+		title: 'Title',
+		description: 'Sample'
+	},
+	{
+		room: 1,
+		number: 6,
+		title: 'Title',
+		description: 'Sample'
+	},
+	{
+		room: 1,
+		number: 7,
+		title: 'Title',
+		description: 'Sample'
+	},
+	{
+		room: 1,
+		number: 8,
+		title: 'Title',
+		description: 'Sample'
+	},
+	{
+		room: 1,
+		number: 9,
+		title: 'Title',
+		description: 'Sample'
+	},
+	{
+		room: 1,
+		number: 10,
+		title: 'Title',
+		description: 'Sample'
+	},
+	{
+		room: 1,
+		number: 11,
+		title: 'Title',
+		description: 'Sample'
+	},
+	{
+		room: 1,
+		number: 12,
+		title: 'Title',
+		description: 'Sample'
+	},
+	{
+		room: 1,
+		number: 13,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 1,
+		number: 14,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 1,
+		number: 15,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 1,
+		number: 16,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 1,
+		number: 17,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 1,
+		number: 18,
+		title: 'Title',
+		description: 'Sample'
+	},
+	{
+		room: 1,
+		number: 19,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 1,
+		number: 20,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 1,
+		number: 21,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 1,
+		number: 22,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 1,
+		number: 23,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 1,
+		number: 24,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 2,
+		number: 1,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 2,
+		number: 2,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 2,
+		number: 3,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 2,
+		number: 4,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 2,
+		number: 5,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 2,
+		number: 6,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 2,
+		number: 7,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 2,
+		number: 8,
+		title: 'Title',
+		description: 'Sample'
+	},
+    
+	{
+		room: 2,
+		number: 9,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 2,
+		number: 10,
+		title: 'Title',
+		description: 'Sample'
+	},
+    
+	{
+		room: 2,
+		number: 11,
+		title: 'Title',
+		description: 'Sample'
+	},
+    
+	{
+		room: 2,
+		number: 12,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 2,
+		number: 13,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 2,
+		number: 14,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 2,
+		number: 15,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 2,
+		number: 16,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 2,
+		number: 17,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 2,
+		number: 18,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 2,
+		number: 19,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 2,
+		number: 20,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 2,
+		number: 21,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 2,
+		number: 22,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 2,
+		number: 23,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 2,
+		number: 24,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+    {
+		room: 3,
+		number: 1,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 3,
+		number: 2,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 3,
+		number: 3,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 3,
+		number: 4,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 3,
+		number: 5,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 3,
+		number: 6,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 3,
+		number: 7,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 3,
+		number: 8,
+		title: 'Title',
+		description: 'Sample'
+	},
+    
+	{
+		room: 3,
+		number: 9,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 3,
+		number: 10,
+		title: 'Title',
+		description: 'Sample'
+	},
+    
+	{
+		room: 3,
+		number: 11,
+		title: 'Title',
+		description: 'Sample'
+	},
+    
+	{
+		room: 3,
+		number: 12,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 3,
+		number: 13,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 3,
+		number: 14,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 3,
+		number: 15,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 3,
+		number: 16,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 3,
+		number: 17,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 3,
+		number: 18,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 3,
+		number: 19,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 3,
+		number: 20,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 3,
+		number: 21,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 3,
+		number: 22,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 3,
+		number: 23,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 3,
+		number: 24,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+    {
+		room: 4,
+		number: 1,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 4,
+		number: 2,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 4,
+		number: 3,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 4,
+		number: 4,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 4,
+		number: 5,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 4,
+		number: 6,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 4,
+		number: 7,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 4,
+		number: 8,
+		title: 'Title',
+		description: 'Sample'
+	},
+    
+	{
+		room: 4,
+		number: 9,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 4,
+		number: 10,
+		title: 'Title',
+		description: 'Sample'
+	},
+    
+	{
+		room: 4,
+		number: 11,
+		title: 'Title',
+		description: 'Sample'
+	},
+    
+	{
+		room: 4,
+		number: 12,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 4,
+		number: 13,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 4,
+		number: 14,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 4,
+		number: 15,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 4,
+		number: 16,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 4,
+		number: 17,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 4,
+		number: 18,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 4,
+		number: 19,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 4,
+		number: 20,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 4,
+		number: 21,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 4,
+		number: 22,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 4,
+		number: 23,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 4,
+		number: 24,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+    {
+		room: 5,
+		number: 1,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 5,
+		number: 2,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 5,
+		number: 3,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 5,
+		number: 4,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 5,
+		number: 5,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 5,
+		number: 6,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 5,
+		number: 7,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 5,
+		number: 8,
+		title: 'Title',
+		description: 'Sample'
+	},
+    
+	{
+		room: 5,
+		number: 9,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 5,
+		number: 10,
+		title: 'Title',
+		description: 'Sample'
+	},
+    
+	{
+		room: 5,
+		number: 11,
+		title: 'Title',
+		description: 'Sample'
+	},
+    
+	{
+		room: 5,
+		number: 12,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 5,
+		number: 13,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 5,
+		number: 14,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 5,
+		number: 15,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 5,
+		number: 16,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 5,
+		number: 17,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 5,
+		number: 18,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 5,
+		number: 19,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 5,
+		number: 20,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 5,
+		number: 21,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 5,
+		number: 22,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 5,
+		number: 23,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 5,
+		number: 24,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+    {
+		room: 6,
+		number: 1,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 6,
+		number: 2,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 6,
+		number: 3,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 6,
+		number: 4,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 6,
+		number: 5,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 6,
+		number: 6,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 6,
+		number: 7,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 6,
+		number: 8,
+		title: 'Title',
+		description: 'Sample'
+	},
+    
+	{
+		room: 6,
+		number: 9,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 6,
+		number: 10,
+		title: 'Title',
+		description: 'Sample'
+	},
+    
+	{
+		room: 6,
+		number: 11,
+		title: 'Title',
+		description: 'Sample'
+	},
+    
+	{
+		room: 6,
+		number: 12,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 6,
+		number: 13,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 6,
+		number: 14,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 6,
+		number: 15,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 6,
+		number: 16,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 6,
+		number: 17,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 6,
+		number: 18,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 6,
+		number: 19,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 6,
+		number: 20,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 6,
+		number: 21,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 6,
+		number: 22,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 6,
+		number: 23,
+		title: 'Title',
+		description: 'Sample'
+	},
+
+	{
+		room: 6,
+		number: 24,
+		title: 'Title',
+		description: 'Sample'
+	},
+];
