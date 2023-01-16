14ec2474d1e291e83d030c378d6d09afb7acd314
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2023-01-16 12:29:21 +0700

    Change css condition

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -12,4 +12,4 @@
     }
 </script>
 
-<button class={`${active && "bg-red-400 text-black"} py-3 px-6 rounded-full border-red-400 text-white border-2`} on:click={sendData}>6/{room}</button>
+<button class={`${active && "bg-red-400"} py-3 px-6 rounded-full border-red-400 text-white border-2`} on:click={sendData}>6/{room}</button>
