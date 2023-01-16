3555357f754b189aa3c68478488955d05c28cead
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2023-01-16 11:51:04 +0700

    Load individual art

---
+++ src/routes/art/[room]/[number]/+page.server.js
@@ -0,0 +1,10 @@
+import { error } from "@sveltejs/kit";
+import { art } from "../../../data";
+
+export function load({ params }) {
+    const ar = art.find((a) => (a.room === parseInt(params.room)) && (a.number === parseInt(params.number)))
+
+    if (!ar) throw error(404)
+
+    return { ar }
+}
