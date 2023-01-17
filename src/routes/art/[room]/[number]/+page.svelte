bd276ca3408e8df4e08e65523785032e93253eed
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2023-01-17 10:09:17 +0700

    Make button disappear when it reach first and last art

--- src/routes/art/[room]/[number]/+page.svelte
+++ src/routes/art/[room]/[number]/+page.svelte
@@ -6,14 +6,14 @@
 	let previous = '';
 	let next = '';
 	$: if (room === 1 && number === 1) {
-		previous = '/art/1/1/#';
+		previous = '';
 	} else if (number === 1) {
 		previous = `/art/${room - 1}/${students[room - 2]}`;
 	} else {
 		previous = `/art/${room}/${number - 1}`;
 	}
 	$: if (room === 6 && number === 24) {
-		next = '/art/6/24/#';
+		next = '';
 	} else if (students[room - 1] === number) {
 		next = `/art/${room + 1}/1`;
 	} else {
@@ -31,12 +31,16 @@
 		<img class="aspect-a3 w-96" src={`/room${room}/number${number}.webp`} alt={data.ar.title} />
 		<div class="w-1/3">
 			<div class="flex gap-5 mb-4">
-				<a href={previous}>
-					<img class="bg-red-400 rounded-full p-3 w-12" src="/arrow_left.svg" alt="Arrow Left" />
-				</a>
-				<a href={next}>
-					<img class="bg-red-400 rounded-full p-3 w-12" src="/arrow_right.svg" alt="Arrow Right" />
-				</a>
+                {#if previous !== ''}
+                    <a href={previous}>
+                        <img class="bg-red-400 rounded-full p-3 w-12" src="/arrow_left.svg" alt="Arrow Left" />
+                    </a>
+                {/if}
+                {#if next !== ''}
+                    <a href={next}>
+                        <img class="bg-red-400 rounded-full p-3 w-12" src="/arrow_right.svg" alt="Arrow Right" />
+                    </a>
+                {/if}
 			</div>
 			<h1 class="font-bold text-4xl pb-4 border-red-400 border-b-2">{data.ar.title}</h1>
 			<h3 class="py-4">{data.ar.description}</h3>
