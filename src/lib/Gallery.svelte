3d0278988c76bad51383bd1ff6673d671120566f
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2023-01-17 22:50:18 +0700

    Use $classroom

--- src/lib/Gallery.svelte
+++ src/lib/Gallery.svelte
@@ -1,53 +1,28 @@
 <script>
 	import Button from './Button.svelte';
 	import Showcase from './Showcase.svelte';
-	let classroom = [
-		{
-			room: 1,
-			active: true
-		},
-		{
-			room: 2,
-			active: false
-		},
-		{
-			room: 3,
-			active: false
-		},
-		{
-			room: 4,
-			active: false
-		},
-		{
-			room: 5,
-			active: false
-		},
-		{
-			room: 6,
-			active: false
-		}
-	];
+	import { classroom } from './store';
 	export let data;
 
 	function handleMessage(event) {
-		classroom.forEach((r) => {
+		$classroom.forEach((r) => {
 			if (r.room === event.detail.room) {
 				r.active = true;
 			} else {
 				r.active = false;
 			}
 		});
-		classroom = classroom;
+		$classroom = $classroom
 	}
 </script>
 
 <div class="flex flex-col items-center -mt-40 sm:-mt-20">
 	<div class="grid grid-cols-3 sm:grid-cols-6 gap-x-4 gap-y-4 flex-wrap justify-center">
-		{#each classroom as { room, active } (room)}
+		{#each $classroom as { room, active } (room)}
 			<Button {room} {active} on:message={handleMessage} />
 		{/each}
 	</div>
-	{#each classroom as { room, active } (room)}
+	{#each $classroom as { room, active } (room)}
 		{#if active}
 			<Showcase {room} {data} />
 		{/if}
