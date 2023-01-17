8ae63b039ffd09f7e50c52d4db793f8f4279e412
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2023-01-17 22:07:04 +0700

    Adjust padding according to their screen size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,9 +16,9 @@
 </svelte:head>
 
 <PageTransition>
-	<div class="bg-art bg-cover py-64 w-full flex flex-col items-center">
+	<div class="bg-art bg-cover py-52 sm:py-64 w-full flex flex-col items-center">
 		<Header />
-		<div class="text-white text-6xl text-left font-bold">
+		<div class="text-white text-4xl sm:text-6xl text-left font-bold">
 			<h1>PCSHSPL</h1>
 			<h1>Y2K Street Art</h1>
 		</div>
