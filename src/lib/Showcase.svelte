41e595ceee0455192a2977eea680293eb58628c2
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2023-01-20 10:43:53 +0700

    Adjust image's width to the max

--- src/lib/Showcase.svelte
+++ src/lib/Showcase.svelte
@@ -5,13 +5,13 @@
 </script>
 
 <div
-	class="whitespace-nowrap overflow-auto sm:grid sm:grid-cols-4 mt-6 w-full sm:place-items-center gap-y-4"
+	class="whitespace-nowrap overflow-auto sm:grid sm:grid-cols-5 mt-6 w-full sm:place-items-center"
 >
 	{#each data.details as detail}
 		{#if detail.room === room}
 			<a class="inline-block" href={`/art/${detail.room}/${detail.number}`}>
 				<img
-					class="aspect-square w-full sm:w-40"
+					class="aspect-square w-full"
 					src={detail.image}
 					alt={detail.title}
 					in:fly={{ y: 200, duration: 2000 }}
