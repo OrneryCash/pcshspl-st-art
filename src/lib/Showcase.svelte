6bb9581ded48db1175ee81e42e897424a4047931
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2023-01-26 12:24:45 +0700

    Add teacher art and make it big

--- src/lib/Showcase.svelte
+++ src/lib/Showcase.svelte
@@ -4,22 +4,41 @@
 	export let data;
 </script>
 
-<div
-	class="whitespace-nowrap overflow-x-scroll overflow-y-hidden snap-mandatory snap-x sm:snap-none sm:grid sm:grid-cols-4 mt-6 w-full sm:place-items-center"
->
-	{#each data.details as detail}
-		{#if detail.room === room}
-			<a
-				class="inline-block snap-center sm:snap-align-none"
-				href={`/art/${detail.room}/${detail.number}`}
-			>
-				<img
-					class="aspect-square w-full"
-					src={detail.image}
-					alt={detail.title}
-					in:fly={{ y: 200, duration: 2000 }}
-				/>
-			</a>
-		{/if}
-	{/each}
-</div>
+{#if room === 7}
+	<div class="flex justify-center w-2/3 items-center mt-8">
+		{#each data.details as detail}
+			{#if detail.room === room}
+				<a
+					href={`/art/${detail.room}/${detail.number}`}
+				>
+					<img
+						class="aspect-square w-full"
+						src={detail.image}
+						alt={detail.title}
+						in:fly={{ y: 200, duration: 2000 }}
+					/>
+				</a>
+			{/if}
+		{/each}
+	</div>
+{:else}
+	<div
+		class="whitespace-nowrap overflow-x-scroll overflow-y-hidden snap-mandatory snap-x sm:snap-none sm:grid sm:grid-cols-4 mt-6 w-full sm:place-items-center"
+	>
+		{#each data.details as detail}
+			{#if detail.room === room}
+				<a
+					class="inline-block snap-center sm:snap-align-none"
+					href={`/art/${detail.room}/${detail.number}`}
+				>
+					<img
+						class="aspect-square w-full"
+						src={detail.image}
+						alt={detail.title}
+						in:fly={{ y: 200, duration: 2000 }}
+					/>
+				</a>
+			{/if}
+		{/each}
+	</div>
+{/if}
