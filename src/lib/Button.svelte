0bc68b70bc2948be9de1047942d82b9aba1c0c13
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2023-01-26 12:23:21 +0700

    Add teacher button

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -12,9 +12,17 @@
 	}
 </script>
 
-<button
-	class={`${
-		active && 'bg-red-400'
-	} py-1.5 sm:py-3 px-5 sm:px-6 rounded-full border-red-400 text-white border-2`}
-	on:click={sendData}>6/{room}</button
->
+{#if room === 7}
+	<button
+		class={`${
+			active && 'bg-red-400'
+		} py-1.5 sm:py-3 px-5 sm:px-6 rounded-full border-red-400 text-white border-2`}
+		on:click={sendData}>T.</button>
+{:else}
+	<button
+		class={`${
+			active && 'bg-red-400'
+		} py-1.5 sm:py-3 px-5 sm:px-6 rounded-full border-red-400 text-white border-2`}
+		on:click={sendData}>6/{room}</button
+	>
+{/if}
