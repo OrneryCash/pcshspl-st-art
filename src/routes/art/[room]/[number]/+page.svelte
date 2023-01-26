b3d37bada9a9901093cb40c0c8209204d63a3702
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2023-01-26 09:29:55 +0700

    Adjust components size and add more information

--- src/routes/art/[room]/[number]/+page.svelte
+++ src/routes/art/[room]/[number]/+page.svelte
@@ -28,10 +28,17 @@
 		</a>
 	</div>
 	<div
-		class="flex flex-col items-center sm:flex-row sm:items-start justify-center gap-8 sm:gap-12 mt-10 w-full"
+		class="flex flex-col items-center md:flex-row md:items-start justify-center gap-8 md:gap-12 mt-10 w-full"
 	>
-		<img class="aspect-square w-96" src={`/room${room}/number${number}.webp`} alt={data.ar.title} />
-		<div class="w-3/4 sm:w-1/3">
+		<div class="flex flex-col items-center w-2/3 md:w-1/3">
+			<img class="aspect-square w-full" src={`/room${room}/number${number}.webp`} alt={data.ar.title} />
+			<div class="text-gray-500 flex flex-col font-bold text-lg">
+				<span>{data.ar.technique}</span>	
+				<span>{data.ar.size}</span>
+			</div>
+		</div>
+
+		<div class="w-3/4 md:w-1/3">
 			<div class="flex gap-5 mb-4">
 				{#if previous !== ''}
 					<a href={previous}>
@@ -50,7 +57,7 @@
 			</div>
 			<h1 class="font-bold text-4xl pb-4 border-red-400 border-b-2">{data.ar.title}</h1>
 			<h3 class="py-4">{data.ar.description}</h3>
-			{#if data.ar.instagram !== ''}
+			{#if data.ar.instagram !== ""}
 				<a
 					class="flex items-center gap-2 bg-gradient-to-r from-yellow-400 via-red-400 to-purple-600 p-4 w-max rounded-full"
 					href={`https://instagram.com/${data.ar.instagram}`}
