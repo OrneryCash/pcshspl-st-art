0cb4eef9d75a09e4e01c4f8ed386a8aa76e1d63b
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2023-05-31 11:06:33 +0700

    Add more csp directives

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,17 @@ const config = {
 			mode: 'hash',
 			directives: {
 				'script-src': ['self'],
-				'frame-ancestors': ['self']
+				'frame-ancestors': ['self'],
+				'connect-src': ["'self'", 'ws://localhost:*'],
+				'img-src': ["'self'", 'data:'],
+				'font-src': ["'self'", 'data:'],
+				'form-action': ["'self'"],
+				'frame-ancestors': ["'self'"],
+				'frame-src': ["'self'"],
+				'manifest-src': ["'self'"],
+				'media-src': ["'self'", 'data:'],
+				'object-src': ["'none'"],
+				'style-src': ["'self'", "'unsafe-inline'"]
 			}
 		}
 	},
